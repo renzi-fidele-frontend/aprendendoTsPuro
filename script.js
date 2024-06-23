c169501727bf33a40520e6e3740f51c6ac279c61
renzi-fidele-frontend <renzifidele2001@gmail.com>
2024-06-23 21:43:13 +0200

    Primeiro commit

---
+++ script.js
@@ -0,0 +1,9 @@
+"use strict";
+const nome = "Renzi";
+const idade = 23;
+console.log(nome);
+function somar(a, b) {
+    console.log(a + b);
+}
+somar(1, 2);
+console.log("Ola maria, tudo bem");
---
+++ script.ts
@@ -0,0 +1,13 @@
+const nome = "Renzi";
+
+const idade = 23;
+
+console.log(nome);
+
+function somar(a: number, b: number) {
+   console.log(a + b);
+}
+
+somar(1, 2);
+
+console.log("Ola maria, tudo bem");
