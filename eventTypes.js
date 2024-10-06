a207c4772b4353c211d0f5b1caef7302958960ec
Renzi Fidele <renzifidele2001@gmail.com>
2024-10-06 23:11:58 +0200

    Aprendendo a utilizar o this para pegar o elemento que disparou o evento

--- eventTypes.js
+++ eventTypes.js
@@ -6,3 +6,11 @@ if (titulo instanceof HTMLElement) {
 function increaseOnClick(event) {
     console.log(event);
 }
+//  Opcional: Podes utilizar o this para pegar o elemento que disparou o evento
+const this_el = document.querySelector(".this");
+if (this_el instanceof HTMLElement) {
+    this_el.addEventListener("click", usarThis);
+}
+function usarThis(event) {
+    console.log(`Clicaste e o texto interior é: ${this.innerText}`);
+}
--- eventTypes.ts
+++ eventTypes.ts
@@ -7,3 +7,13 @@ if (titulo instanceof HTMLElement) {
 function increaseOnClick(event: MouseEvent) {
    console.log(event);
 }
+
+//  Opcional: Podes utilizar o this para pegar o elemento que disparou o evento
+const this_el = document.querySelector(".this");
+if (this_el instanceof HTMLElement) {
+   this_el.addEventListener("click", usarThis);
+}
+
+function usarThis(this: HTMLElement, event: MouseEvent) {
+   console.log(`Clicaste e o texto interior é: ${this.innerText}`);
+}
