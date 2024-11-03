b5f37d336987e08b6a446e6ff0049ecb76a3234d
Renzi Fidele <renzifidele2001@gmail.com>
2024-11-03 23:05:28 +0200

    Aprendendo sobre o operador ...rest

---
+++ destructuring.ts
@@ -0,0 +1,21 @@
+// Operador ...rest
+// Usar quando a função recebe diversos parametros imprevisiveis
+function maiorOuMenor(tipo: "maior" | "menor", ...numeros: number[]) {
+   if (tipo === "maior") {
+      let maior = 0;
+      numeros.forEach((nr) => {
+         if (nr > maior) maior = nr;
+      });
+      return maior;
+   } else {
+      let menor = Infinity;
+      numeros.forEach((nr) => {
+         if (nr < menor) menor = nr;
+      });
+      return menor;
+   }
+}
+
+const maior1 = maiorOuMenor("maior", 2, 3, 4, 10, -1, Infinity);
+const menor1 = maiorOuMenor("menor", -10, 3, 5, 29, 40);
+console.log(maior1, menor1);
