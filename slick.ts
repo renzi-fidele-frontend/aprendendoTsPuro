127dfd674a4211c7e186d8348387feb01de12373
Renzi Fidele <renzifidele2001@gmail.com>
2024-12-08 05:50:35 +0200

    Aprendendo a utilizar uma lib que funciona através de método ao invés da sua instância global

---
+++ slick.ts
@@ -0,0 +1,3 @@
+const swiperEl = $("#mySwiper");
+
+swiperEl.slick({ autoplay: true, slidesToShow: 3, arrows: true });
--- types/global.d.ts
+++ types/global.d.ts
@@ -2,3 +2,12 @@ interface Alimento {
    nome: string;
    preco: number;
 }
+
+// Libs
+
+// 1 - Caso a lib seja utilizada através de um método e não da sua instância global
+interface JQuery {
+   /** The last carousel you'll ever need. */
+   slick: (options?: {}) => void;
+}
+// 2 - Caso declare const slick: any;
