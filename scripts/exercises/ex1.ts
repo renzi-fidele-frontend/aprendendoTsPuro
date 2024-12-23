f837fbb7e56d5f1161ded2e815ae2661634cddd4
Renzi Fidele <renzifidele2001@gmail.com>
2024-12-23 15:47:59 +0200

    // Calculando o total por forma de pagamento

--- scripts/exercises/ex1.ts
+++ scripts/exercises/ex1.ts
@@ -15,7 +15,7 @@ const corpo = document.querySelector("#corpo");
 async function apanharDados() {
    let total = 0;
    let credito = 0;
-   let boleto;
+   let boleto = 0;
    let pago;
    let recusado;
    let aguardando;
@@ -40,8 +40,13 @@ async function apanharDados() {
             });
 
             arrayNormalizado.forEach(({ nome, email, valor, formaDePagamento, status }) => {
+               // Calculando o total
                let numero = Number(valor.split(",")[0]);
                if (!isNaN(numero)) total += numero;
+
+               // Calculando o total por forma de pagamento
+               if (formaDePagamento === "Cartão de Crédito") credito += 1;
+               if (formaDePagamento === "Boleto") boleto += 1;
 
                // Renderizando a lista na tabela
                corpo.innerHTML += `
@@ -60,6 +65,15 @@ async function apanharDados() {
             if (estatisticas) {
                estatisticas.innerHTML += `
                 <h6>Total: <span id="light">R$ ${total}</span></h6>
+                <hr>
+                <h6>Cartão de Crédito: <span id="light">${credito}</span></h6>
+                <h6>Boleto: <span id="light">${boleto}</span></h6>
+                <hr>
+                <h6>Pago: <span id="light">R$ ${pago}</span></h6>
+                <h6>Recusado pela operadora de cartão: <span id="light">R$ ${recusado}</span></h6>
+                <h6>Aguardando pagamento: <span id="light">R$ ${aguardando}</span></h6>
+                <h6>Cancelado: <span id="light">R$ ${cancelado}</span></h6>
+                <h6>Dia da semana com mais vendas: <span id="light">R$ ${melhorDia}</span></h6>
                 `;
             }
          }
